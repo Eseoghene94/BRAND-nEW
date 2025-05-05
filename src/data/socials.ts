cb0b2de0967be8af5449fb299e67c3b27ba538a8
Eseoghene94 <eseoghenedavid1@gmail.com>
2025-05-05 20:39:44 +0100

    modified:   src/data/socials.ts

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,19 +1,19 @@
-import { Social } from '../types';
+import { Social } from "../types";
 
 export const socials: Social[] = [
   {
-    name: 'GitHub',
-    url: 'https://github.com/davidojiyovwi',
-    icon: 'github',
+    name: "GitHub",
+    url: "https://github.com/eseoghene94",
+    icon: "github",
   },
   {
-    name: 'LinkedIn',
-    url: 'https://linkedin.com/in/davidojiyovwi',
-    icon: 'linkedin',
+    name: "LinkedIn",
+    url: "https://linkedin.com/in/davidojiyovwi",
+    icon: "linkedin",
   },
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/davidojiyovwi',
-    icon: 'twitter',
+    name: "Twitter",
+    url: "https://twitter.com/ilovebress",
+    icon: "twitter",
   },
 ];
